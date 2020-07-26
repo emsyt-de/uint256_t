2601a4b50658ae3376d3e557af9a10beb48dd25f
Martin Schuler <martinschuler@posteo.net>
2020-07-26 21:10:24 +0200

    Reimplement string to int conversion as constexpression. Add more constexpr keywords.

--- uint256_t.cpp
+++ uint256_t.cpp
@@ -1,146 +1,11 @@
 #include "uint256_t.h"
-#include <vector>
-#include <cstring>
 #include <sstream>
+#include <vector>
 #include <iomanip>
-#include <iostream>
 
 const uint256_t uint256_0 = 0;
 const uint256_t uint256_1 = 1;
 
-uint256_t uint256_t::multiply(const uint256_t & lhs, const uint256_t & rhs)
-{
-	auto lhs_msb_bit_number = lhs.bits();
-	auto lhs_msb_exponential = uint256_1<<(lhs_msb_bit_number-1);
-	auto rhs_msb_bit_number = rhs.bits();
-	auto rhs_msb_exponential = uint256_1<<(rhs_msb_bit_number-1);
-	if(lhs == uint256_0 || rhs == uint256_0)
-	{
-		return uint256_0;
-	}
-	else if(lhs == lhs_msb_exponential) // Save calculations for 2^n numbers
-	{
-		return rhs << (lhs_msb_bit_number-1);
-	}
-	else if(rhs == rhs_msb_exponential)
-	{
-		return lhs << (rhs_msb_bit_number-1);
-	}
-
-	// split values into 4 64-bit parts
-	uint128_t top[4] = {uint256_t::upper64(lhs.upper128()), uint256_t::lower64(lhs.upper128()), uint256_t::upper64(lhs.lower128()), uint256_t::lower64(lhs.lower128())};
-	uint128_t bottom[4] = {uint256_t::upper64(rhs.upper128()), uint256_t::lower64(rhs.upper128()), uint256_t::upper64(rhs.lower128()), uint256_t::lower64(rhs.lower128())};
-	uint128_t products[4][4];
-
-	// multiply each component of the values
-	for(int y = 3; y > -1; y--){
-		for(int x = 3; x > -1; x--){
-			products[3 - y][x] = top[x] * bottom[y];
-		}
-	}
-
-	// first row
-	uint128_t fourth64 = uint128_t(uint256_t::lower64(products[0][3]));
-	uint128_t third64  = uint128_t(uint256_t::lower64(products[0][2])) + uint128_t(uint256_t::upper64(products[0][3]));
-	uint128_t second64 = uint128_t(uint256_t::lower64(products[0][1])) + uint128_t(uint256_t::upper64(products[0][2]));
-	uint128_t first64  = uint128_t(uint256_t::lower64(products[0][0])) + uint128_t(uint256_t::upper64(products[0][1]));
-
-	// second row
-	third64  += uint128_t(uint256_t::lower64(products[1][3]));
-	second64 += uint128_t(uint256_t::lower64(products[1][2])) + uint128_t(uint256_t::upper64(products[1][3]));
-	first64  += uint128_t(uint256_t::lower64(products[1][1])) + uint128_t(uint256_t::upper64(products[1][2]));
-
-	// third row
-	second64 += uint128_t(uint256_t::lower64(products[2][3]));
-	first64  += uint128_t(uint256_t::lower64(products[2][2])) + uint128_t(uint256_t::upper64(products[2][3]));
-
-	// fourth row
-	first64  += uint128_t(uint256_t::lower64(products[3][3]));
-
-	// combines the values, taking care of carry over
-	return uint256_t(first64 << uint128_64, uint128_0) +
-			uint256_t(uint256_t::upper64(third64), third64 << uint128_64) +
-			uint256_t(second64, uint128_0) +
-			uint256_t(fourth64);
-}
-
-std::pair <uint256_t, uint256_t> uint256_t::divmod(const uint256_t & lhs, const uint256_t & rhs)
-{
-	auto rhs_msb_bit_number = rhs.bits();
-	auto rhs_msb_exponential = uint256_1<<(rhs_msb_bit_number-1);
-	/// Save some calculations
-	if (rhs == uint256_0)
-	{
-		throw std::domain_error("Error: division or modulus by 0");
-	}
-	else if (rhs == uint256_1)
-	{
-		return std::pair <uint256_t, uint256_t> (lhs, uint256_0);
-	}
-	else if (lhs == rhs)
-	{
-		return std::pair <uint256_t, uint256_t> (uint256_1, uint256_0);
-	}
-	else if ((lhs == uint256_0) || (lhs < rhs))
-	{
-		return std::pair <uint256_t, uint256_t> (uint256_0, lhs);
-	}
-	else if(rhs == rhs_msb_exponential) // check if only one bit is set (2^n)
-	{
-		return std::pair(lhs >> (rhs_msb_bit_number-1), lhs & (rhs_msb_exponential-1));
-	}
-
-	std::pair <uint256_t, uint256_t> qr(uint256_0, lhs);
-	auto msb_order_diff = lhs.bits() - rhs_msb_bit_number;
-	uint256_t copyd = rhs << msb_order_diff;
-	uint256_t adder = uint256_1 << msb_order_diff;
-	while (qr.second >= rhs)
-	{
-		if (qr.second >= copyd)
-		{
-			qr.second -= copyd;
-			qr.first |= adder;
-		}
-		copyd >>= uint256_1;
-		adder >>= uint256_1;
-	}
-	return qr;
-}
-
-uint256_t uint256_t::exp(const uint256_t & base, const uint256_t & exponent)
-{
-	auto result = uint256_1;
-	auto l_exp = exponent;
-	while(l_exp > uint128_0)
-	{
-		result *= base;
-		--l_exp;
-	}
-	return result;
-}
-
-/// Get order of msb bit.
-/// Return 0 if value == 0, otherwise [1 ... 256].
-uint16_t uint256_t::bits() const{
-	uint16_t out = 0;
-	if(upper > std::numeric_limits<uint64_t>::max())
-	{
-		out = 256 - static_cast<uint16_t>(__builtin_clzll(upper >> 64)); // upper has to be > 0
-	}
-	else if (upper > 0)
-	{
-		out = 192 - static_cast<uint16_t>(__builtin_clzll(static_cast<uint64_t>(upper))); // upper has to be > 0
-	}
-	else if(lower > std::numeric_limits<uint64_t>::max())
-	{
-		out = 128 - static_cast<uint16_t>(__builtin_clzll(lower >> 64)); // lower has to be > 0
-	}
-	else if(lower > 0)
-	{
-		out = 64 - static_cast<uint16_t>(__builtin_clzll(static_cast<uint64_t>(lower))); // lower has to be > 0
-	}
-	return out;
-}
 
 /// stream operators
 
@@ -202,45 +67,7 @@ std::istream & operator>>(std::istream & stream, uint256_t & rhs)
 {
 	std::string s;
 	stream >> s;
-	std::string_view sv = s;
-	bool is_hex = sv.starts_with("0x") && sv.size() > 2 && sv.size() <= 66 && sv.find_first_not_of("0123456789abcdefABCDEF", 2) == std::string::npos;
-	bool is_oct = sv.starts_with('0') && sv.size() > 1 && sv.size() <= 86 && sv.find_first_not_of("01234567", 1) == std::string::npos;
-	bool is_dec = sv.size() > 0 && sv.size() <= 80 && sv.find_first_not_of("0123456789", 0) == std::string::npos;
-	auto gen_in = [](decltype (rhs) & hs, decltype (sv) & sv, const uint64_t & base, size_t max_size) -> void
-	{
-			hs = uint256_0;
-			int l_base = static_cast<int>(base);
-			std::vector<uint64_t> result;
-			uint256_t exponent = uint256_0;
-			while(sv.size() > 0)
-			{
-				auto size = std::min(sv.size(),max_size);
-				auto num = std::stoul(static_cast<std::string>(sv.substr(sv.size()-size,sv.size())),nullptr,l_base);
-				hs += uint256_t(num) * uint256_t::exp(base,exponent);
-				sv.remove_suffix(size);
-				exponent += size;
-			}
-	};
-	if(is_hex)
-	{
-		// remove hex 0x
-		sv.remove_prefix(2);
-		// extract in batches with max 16^n hex number for 64-bit (2^64 == 16^16)
-		gen_in(rhs,sv,16,16);
-	}
-	else if (is_oct)
-	{
-		// remove oct 0
-		sv.remove_prefix(1);
-		// extract in batches with max 8^n dec number for 64-bit (2^63 == 8^21)
-		gen_in(rhs,sv,8,21);
-	}
-	else if(is_dec)
-	{
-		// extract in batches with max 10^n dec number for 64-bit (10^19)
-		gen_in(rhs,sv,10,19);
-	}
-	else
+	if(!rhs.init(s))
 	{
 		stream.setstate(std::ios::failbit);
 	}
--- uint256_t.h
+++ uint256_t.h
@@ -32,14 +32,13 @@
 	#define __UINT256_T__
 
 	#include <cstdint>
-	#include <ostream>
-	#include <vector>
+	#include <sstream>
 	#include <stdexcept>
-	#include <type_traits>
 	#include <concepts>
 	#include <utility>
 
 	typedef __uint128_t uint128_t;
+	typedef __int128_t int128_t;
 	class uint256_t;
 
 	// Give uint256_t type traits
@@ -54,18 +53,15 @@
 	public:
 
 		/// Constructors
-		uint256_t() = default;
-		uint256_t(const uint256_t & rhs) = default;
-		uint256_t(uint256_t && rhs) = default;
+		constexpr uint256_t() = default;
+		constexpr uint256_t(const uint256_t & rhs) = default;
+		constexpr uint256_t(uint256_t && rhs) = default;
 
 		template <std::integral S, std::integral T>
-		uint256_t(const S & upper_rhs, const T & lower_rhs)
+		constexpr uint256_t(const S & upper_rhs, const T & lower_rhs)
 			: upper(upper_rhs), lower(lower_rhs)
 		{}
 
-		uint256_t(const uint128_t & upper_rhs, const uint128_t & lower_rhs)
-			: upper(upper_rhs), lower(lower_rhs)
-		{}
 		constexpr uint256_t(const uint128_t & lower_rhs)
 			: upper(uint128_0), lower(lower_rhs)
 		{}
@@ -82,6 +78,12 @@
 			lower |= lower_rhs;
 		}
 
+		constexpr uint256_t(const std::string_view & number)
+		{
+			init(number);
+		}
+
+
 		/// Assignment Operators
 		uint256_t & operator=(const uint256_t & rhs) = default;
 		uint256_t & operator=(uint256_t && rhs) = default;
@@ -97,9 +99,19 @@
 			return static_cast<uint8_t>(lower);
 		}
 
+		constexpr operator int8_t() const
+		{
+			return static_cast<int8_t>(lower);
+		}
+
 		constexpr operator uint16_t() const
 		{
 			return static_cast<uint16_t>(lower);
+		}
+
+		constexpr operator int16_t() const
+		{
+			return static_cast<int16_t>(lower);
 		}
 
 		constexpr operator uint32_t() const
@@ -107,14 +119,29 @@
 			return static_cast<uint32_t>(lower);
 		}
 
+		constexpr operator int32_t() const
+		{
+			return static_cast<int32_t>(lower);
+		}
+
 		constexpr operator uint64_t() const
 		{
 			return static_cast<uint64_t>(lower);
 		}
 
+		constexpr operator int64_t() const
+		{
+			return static_cast<int64_t>(lower);
+		}
+
 		constexpr operator uint128_t() const
 		{
 			return lower;
+		}
+
+		constexpr operator int128_t() const
+		{
+			return static_cast<int128_t>(lower);
 		}
 
 		/// Bitwise Operators
@@ -290,7 +317,7 @@
 
 		/// Equal
 		template <std::integral T1, std::integral T2>
-		friend inline bool operator==(const T1 & lhs, const T2 & rhs)
+		friend inline constexpr bool operator==(const T1 & lhs, const T2 & rhs)
 		{
 			if constexpr (std::is_same_v<T1,T2> && std::is_same_v<T1,uint256_t>)
 			{
@@ -339,7 +366,7 @@
 
 		/// Plus
 		template <std::integral T1, std::integral T2>
-		friend inline uint256_t operator+(const T1 & lhs, const T2 & rhs)
+		friend inline constexpr uint256_t operator+(const T1 & lhs, const T2 & rhs)
 		{
 			if constexpr (std::is_same_v<T1,T2> && std::is_same_v<T1,uint256_t>)
 			{
@@ -353,13 +380,13 @@
 		}
 
 		template <std::integral T1, std::integral T2>
-		friend inline T1 & operator+=(T1 & lhs, const T2 & rhs){
+		friend inline constexpr T1 & operator+=(T1 & lhs, const T2 & rhs){
 			return lhs = static_cast <T1> (lhs + rhs);
 		}
 
 		/// Minus
 		template <std::integral T1, std::integral T2>
-		friend inline uint256_t operator-(const T1 & lhs, const T2 & rhs)
+		friend constexpr inline uint256_t operator-(const T1 & lhs, const T2 & rhs)
 		{
 			if constexpr (std::is_same_v<T1,T2> && std::is_same_v<T1,uint256_t>)
 			{
@@ -374,13 +401,13 @@
 
 
 		template <std::integral T1, std::integral T2>
-		friend inline T1 & operator-=(T1 & lhs, const T2 & rhs){
+		friend inline constexpr T1 & operator-=(T1 & lhs, const T2 & rhs){
 			return lhs = static_cast <T1> (lhs - rhs);
 		}
 
 		/// Multiply
 		template <std::integral T1, std::integral T2>
-		friend inline uint256_t operator*(const T1 & lhs, const T2 & rhs){
+		friend inline constexpr uint256_t operator*(const T1 & lhs, const T2 & rhs){
 			if constexpr (std::is_same_v<T1,T2> && std::is_same_v<T1,uint256_t>)
 			{
 				return multiply(lhs,rhs);
@@ -392,13 +419,13 @@
 		}
 
 		template <std::integral T1, std::integral T2 >
-		friend inline T1 & operator*=(T1 & lhs, const T2 & rhs){
+		friend inline constexpr T1 & operator*=(T1 & lhs, const T2 & rhs){
 			return lhs = static_cast <T1> (lhs * rhs);
 		}
 
 		/// Divide
 		template <std::integral T1, std::integral T2>
-		friend inline uint256_t operator/(const T1 & lhs, const T2 & rhs){
+		friend inline constexpr uint256_t operator/(const T1 & lhs, const T2 & rhs){
 			if constexpr (std::is_same_v<T1,T2> && std::is_same_v<T1,uint256_t>)
 			{
 				return uint256_t::divmod(lhs,rhs).first;
@@ -410,13 +437,13 @@
 		}
 
 		template <std::integral T1, std::integral T2 >
-		friend inline T1 & operator/=(T1 & lhs, const T2 & rhs){
+		friend inline constexpr T1 & operator/=(T1 & lhs, const T2 & rhs){
 			return lhs = static_cast <T1> (lhs / rhs);
 		}
 
 		/// Modulo
 		template <std::integral T1, std::integral T2>
-		friend inline uint256_t operator%(const T1 & lhs, const T2 & rhs){
+		friend inline constexpr uint256_t operator%(const T1 & lhs, const T2 & rhs){
 			if constexpr (std::is_same_v<T1,T2> && std::is_same_v<T1,uint256_t>)
 			{
 				return divmod(lhs,rhs).second;
@@ -428,7 +455,7 @@
 		}
 
 		template <std::integral T1, std::integral T2 >
-		friend inline T1 & operator%=(T1 & lhs, const T2 & rhs){
+		friend inline constexpr T1 & operator%=(T1 & lhs, const T2 & rhs){
 			return lhs = static_cast <T1> (lhs % rhs);
 		}
 
@@ -487,26 +514,200 @@
 		friend std::istream & operator>>(std::istream & stream, uint256_t & rhs);
 
 		/// Get private values
-		const uint128_t & upper128() const { return upper; }
-		const uint128_t & lower128() const { return lower; }
+		constexpr uint128_t upper128() const { return upper; }
+		constexpr uint128_t lower128() const { return lower; }
 
-		static inline uint64_t upper64(uint128_t u128) {
+		static inline constexpr uint64_t upper64(uint128_t u128) {
 			return u128>>64;
 		}
 
-		static inline uint64_t lower64(uint128_t u128) {
+		static inline constexpr uint64_t lower64(uint128_t u128) {
 			return static_cast<uint64_t>(u128);
 		}
 
 		/// Get order of msb bit.
 		/// Return 0 if value == 0, otherwise [1 ... 256].
-		uint16_t bits() const;
+		/// Get order of msb bit.
+		/// Return 0 if value == 0, otherwise [1 ... 256].
+		constexpr uint16_t bits() const{
+			uint16_t out = 0;
+			if(upper > std::numeric_limits<uint64_t>::max())
+			{
+				out = 256 - static_cast<uint16_t>(__builtin_clzll(upper >> 64)); // upper has to be > 0
+			}
+			else if (upper > 0)
+			{
+				out = 192 - static_cast<uint16_t>(__builtin_clzll(static_cast<uint64_t>(upper))); // upper has to be > 0
+			}
+			else if(lower > std::numeric_limits<uint64_t>::max())
+			{
+				out = 128 - static_cast<uint16_t>(__builtin_clzll(lower >> 64)); // lower has to be > 0
+			}
+			else if(lower > 0)
+			{
+				out = 64 - static_cast<uint16_t>(__builtin_clzll(static_cast<uint64_t>(lower))); // lower has to be > 0
+			}
+			return out;
+		}
 
-		static uint256_t exp(const uint256_t & base, const uint256_t & exponent);
+		static constexpr uint256_t exp(const uint256_t & base, const uint256_t & exponent)
+		{
+			uint256_t uint256_0 = 0;
+			uint256_t result = 1;
+			auto l_exp = exponent;
+			while(l_exp > uint256_0)
+			{
+				result *= base;
+				--l_exp;
+			}
+			return result;
+		}
 	private:
-		static uint256_t multiply(const uint256_t & lhs, const uint256_t & rhs);
-		static std::pair <uint256_t, uint256_t> divmod(const uint256_t & lhs, const uint256_t & rhs);
-		void init(const std::string_view& s);
+		static constexpr uint256_t multiply(const uint256_t & lhs, const uint256_t & rhs)
+		{
+			uint256_t uint256_0 = 0;
+			uint256_t uint256_1 = 1;
+			auto lhs_msb_bit_number = lhs.bits();
+			auto lhs_msb_exponential = uint256_1<<(lhs_msb_bit_number-1);
+			auto rhs_msb_bit_number = rhs.bits();
+			auto rhs_msb_exponential = uint256_1<<(rhs_msb_bit_number-1);
+			if(lhs == uint256_0 || rhs == uint256_0)
+			{
+				return uint256_0;
+			}
+			else if(lhs == lhs_msb_exponential) // Save calculations for 2^n numbers
+			{
+				return rhs << (lhs_msb_bit_number-1);
+			}
+			else if(rhs == rhs_msb_exponential)
+			{
+				return lhs << (rhs_msb_bit_number-1);
+			}
+
+			// split values into 4 64-bit parts
+			uint128_t top[4] = {uint256_t::upper64(lhs.upper128()), uint256_t::lower64(lhs.upper128()), uint256_t::upper64(lhs.lower128()), uint256_t::lower64(lhs.lower128())};
+			uint128_t bottom[4] = {uint256_t::upper64(rhs.upper128()), uint256_t::lower64(rhs.upper128()), uint256_t::upper64(rhs.lower128()), uint256_t::lower64(rhs.lower128())};
+			uint128_t products[4][4];
+
+			// multiply each component of the values
+			for(int y = 3; y > -1; y--){
+				for(int x = 3; x > -1; x--){
+					products[3 - y][x] = top[x] * bottom[y];
+				}
+			}
+
+			// first row
+			uint128_t fourth64 = uint128_t(uint256_t::lower64(products[0][3]));
+			uint128_t third64  = uint128_t(uint256_t::lower64(products[0][2])) + uint128_t(uint256_t::upper64(products[0][3]));
+			uint128_t second64 = uint128_t(uint256_t::lower64(products[0][1])) + uint128_t(uint256_t::upper64(products[0][2]));
+			uint128_t first64  = uint128_t(uint256_t::lower64(products[0][0])) + uint128_t(uint256_t::upper64(products[0][1]));
+
+			// second row
+			third64  += uint128_t(uint256_t::lower64(products[1][3]));
+			second64 += uint128_t(uint256_t::lower64(products[1][2])) + uint128_t(uint256_t::upper64(products[1][3]));
+			first64  += uint128_t(uint256_t::lower64(products[1][1])) + uint128_t(uint256_t::upper64(products[1][2]));
+
+			// third row
+			second64 += uint128_t(uint256_t::lower64(products[2][3]));
+			first64  += uint128_t(uint256_t::lower64(products[2][2])) + uint128_t(uint256_t::upper64(products[2][3]));
+
+			// fourth row
+			first64  += uint128_t(uint256_t::lower64(products[3][3]));
+
+			// combines the values, taking care of carry over
+			return uint256_t(first64 << uint128_64, uint128_0) +
+					uint256_t(uint256_t::upper64(third64), third64 << uint128_64) +
+					uint256_t(second64, uint128_0) +
+					uint256_t(fourth64);
+		}
+
+		static constexpr std::pair <uint256_t, uint256_t> divmod(const uint256_t & lhs, const uint256_t & rhs)
+		{
+			uint256_t uint256_0 = 0;
+			uint256_t uint256_1 = 1;
+			auto rhs_msb_bit_number = rhs.bits();
+			auto rhs_msb_exponential = uint256_1<<(rhs_msb_bit_number-1);
+			/// Save some calculations
+			if (rhs == uint256_0)
+			{
+				throw std::domain_error("Error: division or modulus by 0");
+			}
+			else if (rhs == uint256_1)
+			{
+				return std::pair <uint256_t, uint256_t> (lhs, uint256_0);
+			}
+			else if (lhs == rhs)
+			{
+				return std::pair <uint256_t, uint256_t> (uint256_1, uint256_0);
+			}
+			else if ((lhs == uint256_0) || (lhs < rhs))
+			{
+				return std::pair <uint256_t, uint256_t> (uint256_0, lhs);
+			}
+			else if(rhs == rhs_msb_exponential) // check if only one bit is set (2^n)
+			{
+				return std::pair(lhs >> (rhs_msb_bit_number-1), lhs & (rhs_msb_exponential-1));
+			}
+
+			std::pair <uint256_t, uint256_t> qr(uint256_0, lhs);
+			auto msb_order_diff = lhs.bits() - rhs_msb_bit_number;
+			uint256_t copyd = rhs << msb_order_diff;
+			uint256_t adder = uint256_1 << msb_order_diff;
+			while (qr.second >= rhs)
+			{
+				if (qr.second >= copyd)
+				{
+					qr.second -= copyd;
+					qr.first |= adder;
+				}
+				copyd >>= uint256_1;
+				adder >>= uint256_1;
+			}
+			return qr;
+		}
+
+		constexpr bool init(std::string_view sv)
+		{
+			bool ret = false;
+			bool is_hex = sv.starts_with("0x") && sv.size() > 2 && sv.size() <= 66 && sv.find_first_not_of("0123456789abcdefABCDEF", 2) == std::string::npos;
+			bool is_oct = sv.starts_with('0') && sv.size() > 1 && sv.size() <= 86 && sv.find_first_not_of("01234567", 1) == std::string::npos;
+			bool is_dec = sv.size() > 0 && sv.size() <= 80 && sv.find_first_not_of("0123456789", 0) == std::string::npos;
+			if(is_hex)
+			{
+				// remove hex 0x
+				sv.remove_prefix(2);
+				// extract in batches with max 16^n hex number for 64-bit (2^64 == 16^16)
+				*this = stoi(sv,16);
+				ret = true;
+			}
+			else if (is_oct)
+			{
+				// remove oct 0
+				sv.remove_prefix(1);
+				// extract in batches with max 8^n dec number for 64-bit (2^63 == 8^21)
+				*this = stoi(sv,8);
+				ret = true;
+			}
+			else if(is_dec)
+			{
+				// extract in batches with max 10^n dec number for 64-bit (10^19)
+				*this = stoi(sv,10);
+				ret = true;
+			}
+			return ret;
+		}
+
+		constexpr uint256_t stoi_impl(std::string_view str, const uint256_t & base, const uint256_t & value = 0)
+		{
+			auto c = str.front();
+			auto i = c >= '0' && c <= '9' ? c - '0' : c >= 'a' && c <= 'f' ? c - 'a' + 10 : c - 'A' + 10;
+			return  str.size() > 0 ? stoi_impl(str.substr(1) , base, i + value * base) : value;
+		}
+
+		constexpr uint256_t stoi(std::string_view str, const uint256_t & base)
+		{
+			return stoi_impl(str,base);
+		}
 
 		inline static constexpr uint128_t uint128_0 = 0;
 		inline static constexpr uint128_t uint128_1 = 1;
